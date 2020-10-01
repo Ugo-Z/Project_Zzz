e9e85461873415f1ff83413fcd15f52caba6689a
Takenori Morio <morikapusan@moritoworks.com>
2020-10-01 11:16:05 +0900

    Add:パスの表示

--- src/jp/co/zzz/userInterface/GuiGridLayout.java
+++ src/jp/co/zzz/userInterface/GuiGridLayout.java
@@ -25,8 +25,8 @@ public class GuiGridLayout extends JPanel implements ActionListener{
         button.addActionListener(this);
         setPreferredSize(new Dimension(1024, 768));
         JTextField input = new JTextField();
-        JLabel iPMessage = new JLabel("ファイルのパスを入力してください");
-        JLabel iKMessage = new JLabel("ハイライトするキーワードを入力してください");
+        JLabel inputPathMessage = new JLabel("ファイルのパスを入力してください");
+        JLabel inputKeywordMessage = new JLabel("ハイライトするキーワードを入力してください");
         inputPath = new JTextField(16);
         inputKeyWord = new JTextField(16);
 
@@ -47,8 +47,8 @@ public class GuiGridLayout extends JPanel implements ActionListener{
         constraints.gridx = 0;	//位置x
         constraints.gridy = 1;	//位置y
         constraints.gridwidth = 1;	//コンポーネントの表示領域のセル数 横
-        layout.setConstraints(iPMessage, constraints);//現在の制約を使い
-        this.add(iPMessage);
+        layout.setConstraints(inputPathMessage, constraints);//現在の制約を使い
+        this.add(inputPathMessage);
 
 
         constraints.gridx = 3;	//位置x
@@ -60,8 +60,8 @@ public class GuiGridLayout extends JPanel implements ActionListener{
         constraints.gridx = 0;	//位置x
         constraints.gridy = 2;	//位置y
         constraints.gridwidth = 1;	//コンポーネントの表示領域のセル数 横
-        layout.setConstraints(iKMessage, constraints);//現在の制約を使い
-        this.add(iKMessage);
+        layout.setConstraints(inputKeywordMessage, constraints);//現在の制約を使い
+        this.add(inputKeywordMessage);
 
         constraints.gridx = 3;	//位置x
         constraints.gridy = 2;	//位置y
@@ -90,9 +90,10 @@ public class GuiGridLayout extends JPanel implements ActionListener{
         //表示用のHTML文字列を宣言、先頭に<html>タグの追加
         StringBuilder htmlLine = new StringBuilder();
         htmlLine.append("<html>");
+        htmlLine.append("パス：" + inputPath.getText() + "<br>");
         htmlLine.append("単語数：" + wordCounter);
         htmlLine.append("文字数：" + charCounter);
-        htmlLine.append("<br>");
+        htmlLine.append("<br><br>");
         //タブチェックから出力されたArrayListの全行末尾に<br>を追加し、連結
         for(String line : lines) {
             htmlLine.append(line + "<br>");
